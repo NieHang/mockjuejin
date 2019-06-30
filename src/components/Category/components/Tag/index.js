08e80c0709b796440ba8f82c568d9868f2d4114a
NieHang <1076883711@qq.com>
2019-06-30 11:25:01 +0800

    Categpory子组件Tag

---
+++ src/components/Category/components/Tag/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux'
+
+import { actionCreators } from '../../store'
+
+const Tag = props => {
+	const { path, tag, setTagId } = props;
+
+	return (
+		<div className="category_popover">
+			<ul className="tag_list">
+				{tag.map(item => (
+					<li className="tag" key={item.tagId}>
+						<Link
+							to={'/' + path + '/' + item.title}
+							onClick={() => setTagId(item.tagId)}
+						>
+							{item.title}
+						</Link>
+					</li>
+				))}
+			</ul>
+		</div>
+	);
+};
+
+const mapState = state => ({
+	tag: state.getIn(['category', 'tag']),
+	tagId: state.getIn(['category', 'tagId']),
+})
+
+const mapDispatch = dispatch => ({
+	setTagId(tagId) {
+		dispatch(actionCreators.setTagId(tagId))
+	}
+})
+
+export default connect(mapState, mapDispatch)(Tag);
